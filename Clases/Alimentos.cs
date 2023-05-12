8fa46a28edb25c16f03f900331d7141b7000b1a4
David Roca <darota@floridauniversitaria.es>
2023-05-12 18:32:23 +0200

    Añadiendo clases: alimentos, dieta, platos y valoraciones

---
+++ Clases/Alimentos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Clases
+{
+    class Alimentos
+    {
+        private int id;
+        private string nombre;
+        private double valorNutri;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public double ValorNutri { get { return valorNutri; } set { valorNutri = value; } }
+
+
+        public Alimentos(int id, string nom, double vNutri)
+        {
+            this.id = id;
+            nombre = nom;
+            valorNutri = vNutri;
+        }
+
+        public Alimentos() { }
+    }
+}
