bd5ad9b0bfef3080e04d6f6e6f307cdec8f654e9
Alejandrotos <114495061+Alejandrotos@users.noreply.github.com>
2023-05-12 18:37:35 +0200

    Añadiendo Clase ConexionBD
    Si

---
+++ Clases/ConexionBD.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AEV7
+{
+    // Clase que implementa el patrón Singleton para la conexión a la base de datos
+    public class ConexionBD
+    {
+        private static MySqlConnection instancia = null;
+        private static readonly object padlock = new object();
+
+        private ConexionBD() { }
+
+        // Propiedad pública y estática que devuelve la instancia única de la conexión a la base de datos
+        public static MySqlConnection Conexion
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    // Si la instancia no ha sido creada, se crea y se configura la cadena de conexión
+                    if (instancia == null)
+                    {
+                        instancia = new MySqlConnection();
+                        string server = "server=127.0.0.1;";
+                        string database = "database=aev7;";
+                        string usuario = "uid=root;";
+                        string password = "pwd=;";
+                        instancia.ConnectionString = server + database + usuario + password;
+                    }
+                    return instancia;
+                }
+            }
+        }
+
+        public static void AbrirConexion()
+        {
+            if (instancia != null)
+                instancia.Open();
+        }
+
+        public static void CerrarConexion()
+        {
+            if (instancia != null)
+                instancia.Close();
+        }
+
+    }
+}
