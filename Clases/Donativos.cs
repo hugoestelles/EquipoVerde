5e6a2b0f08c23e203996019de21ef6545f733394
HuGo0oX <huesga@floridauniversitaria.es>
2023-05-12 18:29:51 +0200

    Añadiendo clases

---
+++ Clases/Donativos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Clases
+{
+    class Donativos
+    {
+        private int idDonativo;
+        private double cant;
+        private string nomUser;
+        private DateTime fecha;
+
+        public int IdDonativo { get { return idDonativo; } }
+        public double Cant { get { return cant; } }
+        public string NomUser { get { return nomUser; } }
+        public DateTime Fecha { get { return fecha;} }
+
+        public Donativos(int id, double canti, string nom, DateTime fech)
+        {
+            idDonativo  = id;
+            cant = canti;
+            nomUser = nom;
+            fecha = fech;
+        }
+
+    }
+}
