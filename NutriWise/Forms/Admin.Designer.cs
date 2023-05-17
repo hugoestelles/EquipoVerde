a7e940dc4095761f222951a9e89b0ea32dc93e0a
Oscar Mercado <osmeal@floridauniversitaria.es>
2023-05-17 18:39:29 +0200

    Edicion de texto en admin

--- NutriWise/Forms/Admin.Designer.cs
+++ NutriWise/Forms/Admin.Designer.cs
@@ -334,7 +334,7 @@
             // 
             this.lblElimCorreo2.AutoSize = true;
             this.lblElimCorreo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblElimCorreo2.Location = new System.Drawing.Point(541, 347);
+            this.lblElimCorreo2.Location = new System.Drawing.Point(780, 302);
             this.lblElimCorreo2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblElimCorreo2.Name = "lblElimCorreo2";
             this.lblElimCorreo2.Size = new System.Drawing.Size(95, 25);
