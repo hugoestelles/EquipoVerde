e54013a2b8c00e34d6ed307224b26e6cca493cd1
David Roca <darota@floridauniversitaria.es>
2023-05-22 16:28:13 +0200

    Descomentando id valoracion

--- NutriWise/Clases/Valoracion.cs
+++ NutriWise/Clases/Valoracion.cs
@@ -6,12 +6,12 @@ namespace NutriWise.Clases
 {
     internal class Valoracion
     {
-        //private int id;
+        private int id;
         private int numEstrellas;
         private DateTime fecha;
         private string comentario;
 
-        // public int Id { get { return id; } set { id = value; } }
+        public int Id { get { return id; } set { id = value; } }
         public int NumEstrellas { get { return numEstrellas; } set { numEstrellas = value; } }
         public DateTime Fecha { get { return fecha; } set { fecha = value; } }
         public string Comentario { get { return comentario; } set { comentario = value; } }
