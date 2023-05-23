bc5799cc979ea113b7573a454240ecd71bcbb2e7
Barikelo <barikelop@gmail.com>
2023-05-23 21:39:15 +0200

    añadiendo font awesome, prueba1

--- Pagina-Principal/pagina_principal_en.php
+++ Pagina-Principal/pagina_principal_en.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@18657a9/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" href="./style.css">
     <script type="text/javascript">
